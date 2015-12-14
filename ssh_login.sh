2663c497a97efbfac354a99d658c0fdf8ab2b2b2
James Kominick <james.kominick@gmail.com>
2015-12-14 13:30:04 -0500

    Delete ssh_login.sh
    replaced with python ssh_login.py

--- ssh_login.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# add /path/to/this/file at the end of /etc/bash.bashrc
-
-if [[ -n $SSH_CONNECTION ]] && [[ -z $VIM ]] ; then
-    /home/james/.login/push_login/push_login.py
-fi
-
-
